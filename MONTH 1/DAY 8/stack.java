0c57bda92d30d9aab0f3be09d9ad8920d66e7bea
JAY <53890256+jkrathod2601@users.noreply.github.com>
2022-01-12 01:19:47 -0800

    Develop (#11)
    * DAY 4
    
    * Day 4
    
    * DAY 5
    
    * DAY 5
    
    * DAY 5
    
    * DAY 6
    
    * DAY 6
    
    * DAY 6
    
    * DAY 6
    
    * DAY 6
    
    * DAY 7
    
    * DAY 7
    
    * DAY 7
    
    * DAY 7 FINAL SUBMIT
    
    * DAY 8
    
    * DAY 8

---
+++ MONTH 1/DAY 8/stack.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class stack {
+    public static int a[];
+    public static int top=0;
+    public static int x;
+    public static void push(int n){
+        if(top==5) {
+            System.out.println("stack is full");
+            return;
+        }
+        a[top]=n;
+        top++;
+    }
+    public static int pop(){
+        top--;
+        x=a[top];
+        return x;
+    }
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        a=new int[5];
+        int n,n1;
+        while(true){
+            System.out.println("1 for push::2 for pop:3 for print");
+            n=in.nextInt();
+            if(n==1){
+                System.out.println("enter your element for push");
+                n1=in.nextInt();
+                push(n1);
+            }
+            else if (n==2){
+                if(top>0){
+                    System.out.println(pop()+"this elemet is pop");
+                }else{
+                    System.out.println("first push the elements");
+                }
+            }
+            else if(n==3){
+                for(int i=0;i<top;i++){
+                    System.out.println(a[i]);
+                }
+            }
+            else{
+                System.out.println("enter the right elements");
+            }
+
+        }
+    }
+
+}
