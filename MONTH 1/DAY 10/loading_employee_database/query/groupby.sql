e48ebdaa5c50d140b0d2404b3f0d294b2ed7af0f
jayrathod <jkrathod2601@gmail.com>
2022-01-17 17:41:49 +0530

    DAY 10

--- MONTH 1/DAY 10/loading_employee_database/query/groupby.sql
+++ MONTH 1/DAY 10/loading_employee_database/query/groupby.sql
@@ -11,3 +11,19 @@
 select first_name , count(first_name) as c1 from employees group by first_name;
 
 -- alise means rename your column name in exexuting te querey and AS is the syntax
+
+-- having is only use in aggregate function in groupby
+
+select first_name , count(first_name) as c1 from employees 
+group by first_name having count(first_name)>250 order by first_name asc;
+
+-- task 
+SELECT 
+    first_name, COUNT(first_name) as c1
+FROM
+    employees
+WHERE
+    hire_date > '1999-01-01'
+GROUP BY first_name
+HAVING COUNT(first_name) < 200
+ORDER BY c1 ASC;
---
+++ MONTH 1/DAY 10/loading_employee_database/query/limit.sql
@@ -0,0 +1,2 @@
+-- limit is use for the data retriving in limited manner
+select * from employees where gender="M" limit 5; 
