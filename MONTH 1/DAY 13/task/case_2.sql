2de9b59fe0d72f1db3395b280fb8527602539c81
jayrathod <jkrathod2601@gmail.com>
2022-01-20 15:45:57 +0530

    DAY 13

--- MONTH 1/DAY 13/task/case_2.sql
+++ MONTH 1/DAY 13/task/case_2.sql
@@ -1,28 +1,3 @@
-create database if not exists testting_day13;
-use testting_day13;
-
-create table Person(
- id int auto_increment not null primary key,
- email varchar(20) not null
-);
-
-insert into person(email)
-values ("a@b.com"),
-("c&d.com"),
-("A@B.com"),
-("c&d.com"),
-("d&c.com");
-
-select * from person;
-
-select email
-from person
-group by lower(email)
-having count(email)>1;
-
-
--- --problem --2
-
 create table department (
 	id int auto_increment primary key,
     name varchar(20) not null
@@ -51,8 +26,3 @@ where 3 > (select count(distinct(e2.Salary))
                   where e2.Salary > e1.Salary 
                   and e1.DepartmentId = e2.DepartmentId
                   );
-
-
-
-
-
