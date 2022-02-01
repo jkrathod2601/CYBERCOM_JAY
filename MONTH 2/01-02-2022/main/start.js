037ab2c6ca90782e20aa9a1fcdf673d54ab73031
JAY <53890256+jkrathod2601@users.noreply.github.com>
2022-02-01 18:51:40 +0530

    Extra Work JS

---
+++ MONTH 2/01-02-2022/main/start.js
@@ -0,0 +1,27 @@
+let x=5
+let y="10"
+
+console.log(y+x) //105
+
+console.log(x**y) //9765625
+
+console.log(x-y) //-5
+
+let y1;
+console.log(typeof(y1))
+
+// null is object
+
+let xx="jay"
+
+if(xx){
+    console.log("done")
+}
+
+const name= 13>5 ? "jay" :"rathod"
+console.log(name)
+
+console.log("5"<10)
+
+let yy="2022-01-02"
+console.log(yy.slice(0,4))
