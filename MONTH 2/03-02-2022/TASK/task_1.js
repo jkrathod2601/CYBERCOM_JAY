c4927eb16a9eb2c4dc56f0dd72e60d060257053b
rj-riddhi <riddhiparmar2311@gmail.com>
2022-02-03 16:18:31 +0530

    Update task

--- MONTH 2/03-02-2022/TASK/task_1.js
+++ MONTH 2/03-02-2022/TASK/task_1.js
@@ -1,9 +1,9 @@
 // define mass of both
-const mass_mark = 70
-const mass_john = 80
+const mass_mark = parseFloat(prompt("enter the mass of mark"))
+const mass_john = parseFloat(prompt("enter the mass of john"))
     // define height of both
-const height_mark = 1.5
-const height_john = 1.3
+const height_mark = parseFloat(prompt("enter the height of mark"))
+const height_john = parseFloat(prompt("enter the height of john"))
     //define mark's bmi is grater then john 
 let mark_grater_john = true
     //define BMi calculating function
@@ -13,6 +13,7 @@ const findbmi = (mass, height) => {
     //find bmi for both
 const bmi_mark = findbmi(mass_mark, height_mark)
 const bmi_john = findbmi(mass_john, height_john)
+console.log(bmi_mark, bmi_john)
 
 //if john's bmi is grater or equal to mark then flase the
 if (bmi_mark <= bmi_john) {
--- MONTH 2/03-02-2022/TASK/task_2.js
+++ MONTH 2/03-02-2022/TASK/task_2.js
@@ -1,6 +1,17 @@
-let john_team_score = [89, 120, 103]
-let mike_team_score = [116, 94, 123]
+let john_team_score = []
+let mike_team_score = []
+let mary_team_score = []
 
+let n = parseInt(prompt("enter match played for three player"))
+for (let i = 0; i < n; i++) {
+    john_team_score.push(parseInt(prompt("enter score of john")))
+}
+for (let i = 0; i < n; i++) {
+    mike_team_score.push(parseInt(prompt("enter score of mike")))
+}
+for (let i = 0; i < n; i++) {
+    mary_team_score.push(parseInt(prompt("enter score of mary")))
+}
 const find_avg_score = (score) => {
     let sum = 0
     for (let i of score) {
@@ -11,6 +22,7 @@ const find_avg_score = (score) => {
 
 let avg_john = find_avg_score(john_team_score)
 let avg_mike = find_avg_score(mike_team_score)
+let avg_mary = find_avg_score(mary_team_score)
 
 if (avg_john > avg_mike) {
     document.getElementById("ans").innerText = "winner is john"
@@ -21,15 +33,15 @@ if (avg_john > avg_mike) {
 }
 
 //extra task
-let mary_team_score = [97, 134, 105]
-let avg_mary = find_avg_score(mary_team_score)
+
+
 
 if (avg_mary > avg_mike && avg_mary > avg_john) {
     document.getElementById("ans2").innerText = "mary is winner"
 } else if (avg_mike > avg_mary && avg_mik > avg_john) {
-    document.getElementById("ans").innerText = "mike  is winner"
+    document.getElementById("ans2").innerText = "mike  is winner"
 } else if (avg_john > avg_mary && avg_john > avg_mike) {
-    document.getElementById("ans").innerText = "john is winner"
+    document.getElementById("ans2").innerText = "john is winner"
 } else {
-    document.getElementById("ans").innerText = "match is tie"
+    document.getElementById("ans2").innerText = "match is tie"
 }
