615c0878997bf8ff447a1e79434393b80722e3fc
rj-riddhi <riddhiparmar2311@gmail.com>
2022-02-03 18:02:59 +0530

    Final submit

--- MONTH 2/03-02-2022/main.js
+++ MONTH 2/03-02-2022/main.js
@@ -7,3 +7,23 @@ let arr = [1, 2, 3]
 arr.forEach((val) => {
     console.log(fun(val))
 })
+
+const data = ["jayra", "jayraj", "jayp", "jayka"]
+
+// find the first occurance
+let ans = data.find(n => {
+    return n.includes("jayr")
+})
+console.log(ans)
+
+//find all occurance
+ans = data.filter(n => {
+    return n.includes("jayr")
+})
+console.log(ans)
+
+var a = 5;
+
+if (a = 3) {
+    console.log('a', a);
+}
