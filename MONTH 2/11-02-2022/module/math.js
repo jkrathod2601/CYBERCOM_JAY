ea97f84a9c49858f729ddcc2ec4b019161093dd1
jayrathod26 <jkrathod2601@gmail.com>
2022-02-14 09:18:38 +0530

    Final Upload

---
+++ MONTH 2/11-02-2022/module/math.js
@@ -0,0 +1,6 @@
+let sumAll = (a, b) => {return a + b;}
+let subtractAll = (a, b) => {return a - b;}
+let divideAll = (a, b) => {return a / b;}
+let multiplyAll = (a, b) => {return a * b;}
+let findModulus = (a, b) => {return a % b;}
+export {sumAll, subtractAll, divideAll, multiplyAll, findModulus};
